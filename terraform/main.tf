a3bd73fa889bf472899ce0eb78b20f45c30ea8cd
David Vílchez <dvilchez@xuaps.com>
2021-12-31 18:28:08 +0100

    Add terraform script

---
+++ terraform/main.tf
@@ -0,0 +1,100 @@
+terraform {
+  required_providers {
+    azurerm = {
+      version = "=2.20.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
+
+variable "location" {}
+
+variable "region" {
+  type    = string
+  default = "we"
+}
+
+variable "prefix" {
+  type    = string
+  default = "xuaps"
+}
+
+variable "resourceFunction" {
+  type = string
+}
+
+variable "environment" {
+  type = string
+}
+
+variable "storageAccountSku" {
+  default = {
+    tier = "Standard"
+    type = "GRS"
+  }
+}
+
+variable "apimSku" {
+  type = string
+}
+
+variable "apimSkuCapacity" {
+  type = number
+}
+
+variable "apimPublisherName" {
+  type = string
+}
+
+variable "apimPublisherEmail" {
+  type = string
+}
+
+locals {
+  resourceGroupName  = "${var.prefix}-${var.resourceFunction}-${var.environment}-${var.region}"
+  storageAccountName = "${var.prefix}${var.resourceFunction}sa${var.environment}${var.region}"
+  apimName           = "${var.prefix}-${var.resourceFunction}-${var.environment}-${var.region}"
+  kvName             = "${var.prefix}-${var.resourceFunction}-kv-${var.environment}-${var.region}"
+  appInsightsName    = "${var.prefix}-${var.resourceFunction}-appinsights-${var.environment}-${var.region}"
+}
+
+# --- Get reference to logged on Azure subscription ---
+data "azurerm_client_config" "current" {}
+
+# create resource group
+resource "azurerm_resource_group" "rg" {
+  name     = local.resourceGroupName
+  location = var.location
+}
+
+# Create Storage Account
+resource "azurerm_storage_account" "sa" {
+  name                      = local.storageAccountName
+  resource_group_name       = azurerm_resource_group.rg.name
+  location                  = azurerm_resource_group.rg.location
+  account_tier              = var.storageAccountSku.tier
+  account_replication_type  = var.storageAccountSku.type
+  account_kind              = "StorageV2"
+  enable_https_traffic_only = true
+
+  static_website {
+    index_document = "index.html"
+  }
+}
+
+#Add index.html to blob storage
+resource "azurerm_storage_blob" "web" {
+  name                   = "index.html"
+  storage_account_name   = azurerm_storage_account.sa.name
+  storage_container_name = "$web"
+  type                   = "Block"
+  content_type           = "text/html"
+  source                 = "../dist/index.html"
+}
+
+output "storageAccountName" {
+  value = azurerm_storage_account.sa.name
+}
---
+++ terraform/main.tfvars
@@ -0,0 +1,10 @@
+location         = "westeurope"
+prefix           = "xuaps"
+environment      = "poduction"
+region           = "we"
+resourceFunction = "st"
+
+apimSku            = "Developer"
+apimSkuCapacity    = 1
+apimPublisherName  = "Xuaps"
+apimPublisherEmail = "admin@xuaps.com"
