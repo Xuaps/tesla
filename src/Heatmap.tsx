2e95bb9804562f36e9ce7eef96cd2b2f82e76dc6
David Vílchez <dvilchez@xuaps.com>
2022-01-21 21:25:11 +0100

    Change colors

--- src/Heatmap.tsx
+++ src/Heatmap.tsx
@@ -6,45 +6,10 @@ const options: ApexOptions = {
     chart: {
         type: 'heatmap',
     },
-    plotOptions: {
-        heatmap: {
-            shadeIntensity: 0.5,
-            radius: 0,
-            useFillColorAsStroke: true,
-            colorScale: {
-                ranges: [
-                    {
-                        from: 0,
-                        to: 0.15,
-                        name: 'low',
-                        color: '#ffffff',
-                    },
-                    {
-                        from: 0.15,
-                        to: 0.3,
-                        name: 'medium',
-                        color: '#4e5265',
-                    },
-                    {
-                        from: 0.3,
-                        to: 0.75,
-                        name: 'high',
-                        color: '#fff200',
-                    },
-                    {
-                        from: 0.75,
-                        to: 5,
-                        name: 'extreme',
-                        color: '#ff6800',
-                    },
-                ],
-            },
-        },
-    },
     dataLabels: {
         enabled: false,
     },
-    colors: ['#008FFB'],
+    colors: ['#1c3397'],
     title: {
         text: 'Consumo por día y hora',
     },
