9af9852403b495e2879cf9e8646e6e44dcf1527f
David Vílchez <dvilchez@xuaps.com>
2022-01-25 16:54:17 +0100

    Remove reserved from terraform config

--- terraform/main.tf
+++ terraform/main.tf
@@ -112,7 +112,6 @@ resource "azurerm_app_service_plan" "app_service_plan" {
   resource_group_name = azurerm_resource_group.rg.name
   location            = var.location
   kind                = "FunctionApp"
-  reserved = true # this has to be set to true for Linux. Not related to the Premium Plan
   sku {
     tier = "Dynamic"
     size = "Y1"
