b25ab62a8a449decf49ec63f7e8fb328a56d00f4
David Vílchez <dvilchez@xuaps.com>
2022-01-28 22:11:00 +0100

    Reorganize multi repo

--- app/webpack.config.js
+++ app/webpack.config.js
@@ -2,7 +2,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const path = require('path');
 
 module.exports = {
-    entry: './src/app/index.ts',
+    entry: './src/index.ts',
     devtool: 'source-map',
     mode: 'development',
     output: {
