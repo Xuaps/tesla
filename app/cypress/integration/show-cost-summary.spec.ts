847c4863becf6c27453de81314a7fa4e65b25b7e
David Vílchez <dvilchez@xuaps.com>
2022-02-06 12:12:50 +0100

    Mock prices calls

--- app/cypress/integration/show-cost-summary.spec.ts
+++ app/cypress/integration/show-cost-summary.spec.ts
@@ -1,8 +1,18 @@
 describe('Load CNMC consumptions', () => {
     it('show a consumption graph', () => {
+        cy.intercept('/prices/2.0TD/*', (req) => {
+            const urlParts = req.url.split('/');
+            const date = urlParts[urlParts.length - 1].replace('.json', '');
+            const prices = [...(Array(24) as any).keys()].reduce(
+                (acc, hour) => ({ ...acc, [`${date}T${('0' + hour).slice(-2)}:00:00+00:00`]: 0.123 }),
+                {},
+            );
+
+            req.reply(prices);
+        });
         cy.visit('http://localhost:3000');
         cy.get('input[type="file"]').attachFile('consumptions.csv');
 
-        cy.get('.summary').contains('57,51');
+        cy.get('.summary').contains('26,48');
     });
 });
