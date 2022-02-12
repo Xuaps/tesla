9fac0ac6694cf31a4721e1df3e90243510a3c85d
David Vílchez <dvilchez@xuaps.com>
2022-02-12 12:14:18 +0100

    Simplify price addition

--- app/src/actions/index.ts
+++ app/src/actions/index.ts
@@ -1,106 +1,108 @@
 import Papa from 'papaparse';
 
 export type Data = {
-    CUPS: string;
-    Fecha: string;
-    Hora: string;
-    Consumo: string;
-    Metodo_obtencion: string;
+  CUPS: string;
+  Fecha: string;
+  Hora: string;
+  Consumo: string;
+  Metodo_obtencion: string;
 };
 
 export type Consumption = {
-    [date: string]: {
-        [k: number]: {
-            consumption: number;
-            cost: number | undefined;
-        };
+  [date: string]: {
+    [k: number]: {
+      consumption: number;
+      cost: number | undefined;
     };
+  };
 };
 
 export type Prices = {
-    [key: string]: number;
+  [key: string]: number;
 };
 
 //calculations ----
 export const parseCSV = (file: File) => () => {
-    return new Promise<Data[]>((resolve, reject) => {
-        Papa.parse<Data, File>(file, {
-            header: true,
-            skipEmptyLines: true,
-            complete: (res) => resolve(res.data),
-            error: reject,
-        });
+  return new Promise<Data[]>((resolve, reject) => {
+    Papa.parse<Data, File>(file, {
+      header: true,
+      skipEmptyLines: true,
+      complete: (res) => resolve(res.data),
+      error: reject,
     });
+  });
 };
 
 export const toConsumptions = (data: Data[]): Consumption => {
-    return data.reduce(
-        (acc: Consumption, row) => ({
-            ...acc,
-            [row.Fecha]: {
-                ...(acc[row.Fecha] || {}),
-                [row.Hora]: { consumption: parseFloat(row.Consumo.replace(',', '.')) },
-            },
-        }),
-        {},
-    );
+  return data.reduce(
+    (acc: Consumption, row) => ({
+      ...acc,
+      [row.Fecha]: {
+        ...(acc[row.Fecha] || {}),
+        [row.Hora]: { consumption: parseFloat(row.Consumo.replace(',', '.')) },
+      },
+    }),
+    {},
+  );
 };
 
 const formatDate = (date: string): string => {
-    const [day, month, year] = date.split('/');
+  const [day, month, year] = date.split('/');
 
-    return `${year}-${month}-${day}`;
+  return `${year}-${month}-${day}`;
 };
 
 const toISOString = (date: string, hour: number): string => {
-    const [day, month, year] = date.split('/');
+  const [day, month, year] = date.split('/');
 
-    const dateObj = new Date(`${year}/${month}/${day}`);
-    dateObj.setHours(hour);
+  const dateObj = new Date(`${year}/${month}/${day}`);
+  dateObj.setHours(hour);
 
-    return dateObj.toISOString().replace('.000Z', '+00:00');
+  return dateObj.toISOString().replace('.000Z', '+00:00');
 };
 
 const adjustCNMCHour = (hour: string) => parseInt(hour) - 1;
 
 const pricesSearcher = (prices: Prices[]) => {
-    const pricesFlattened = prices.reduce((acc, price) => ({ ...acc, ...price }), {} as Prices);
-    return (date: string, hour: string): number => pricesFlattened[toISOString(date, adjustCNMCHour(hour))];
+  const pricesFlattened = prices.reduce((acc, price) => ({ ...acc, ...price }), {} as Prices);
+  return (date: string, hour: string): number => pricesFlattened[toISOString(date, adjustCNMCHour(hour))];
 };
+
+const consumptionSearcher =
+  (consumptions: Consumption) =>
+  (date: string, hour: string): number =>
+    consumptions[date][parseInt(hour)].consumption;
 
 //actions ----
 const fetchPrices = async (date: string): Promise<Prices> => {
-    const response = await fetch(`prices/2.0TD/${formatDate(date)}.json`);
-    try {
-        return await response.json();
-    } catch {
-        console.log(`Prices for ${date} couldn't be found`);
-        return {};
-    }
+  const response = await fetch(`prices/2.0TD/${formatDate(date)}.json`);
+  try {
+    return await response.json();
+  } catch {
+    console.log(`Prices for ${date} couldn't be found`);
+    return {};
+  }
 };
 
-export const getConsumptionsWithPrice = (consumptions: Consumption) => async () => {
-    return Promise.all(Object.keys(consumptions).map(fetchPrices)).then((prices) => {
-        const lookupPrices = pricesSearcher(prices);
-        return Object.keys(consumptions).reduce((acc, date) => {
-            return {
-                ...acc,
-                [date]: {
-                    ...acc[date],
-                    ...Object.keys(acc[date]).reduce(
-                        (acc, hour) => ({
-                            ...acc,
-                            [parseInt(hour)]: {
-                                ...acc[parseInt(hour)],
-                                cost:
-                                    lookupPrices(date, hour) &&
-                                    lookupPrices(date, hour) * acc[parseInt(hour)].consumption,
-                            },
-                        }),
-                        acc[date],
-                    ),
-                },
-            };
-        }, consumptions);
-    });
+export const getConsumptionsWithPrice = (consumptions: Consumption) => async (): Promise<Consumption> => {
+  return Promise.all(Object.keys(consumptions).map(fetchPrices)).then((prices) => {
+    const lookupPrices = pricesSearcher(prices);
+    const lookupConsumptions = consumptionSearcher(consumptions);
+
+    return Object.keys(consumptions).reduce((consumptionsWithPrices, date) => {
+      return {
+        ...consumptionsWithPrices,
+        [date]: Object.keys(consumptions[date]).reduce(
+          (consumptionsByDayWithPrices, hour) => ({
+            ...consumptionsByDayWithPrices,
+            [parseInt(hour)]: {
+              consumption: lookupConsumptions(date, hour),
+              cost: lookupPrices(date, hour) && lookupPrices(date, hour) * lookupConsumptions(date, hour),
+            },
+          }),
+          {},
+        ),
+      };
+    }, {});
+  });
 };
