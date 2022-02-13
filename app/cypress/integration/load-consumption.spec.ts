0c91f436f628b3c6e0feb50f9e0028105c91ba9b
David Vílchez <dvilchez@xuaps.com>
2022-02-13 14:29:07 +0100

    Add expect to cypress test

--- app/cypress/integration/load-consumption.spec.ts
+++ app/cypress/integration/load-consumption.spec.ts
@@ -1,8 +1,9 @@
 describe('Load CNMC consumptions', () => {
-    it('show a consumption graph', () => {
-        cy.visit('http://localhost:3000');
-        cy.get('input[type="file"]').attachFile('consumptions.csv');
+  it('show a consumption graph', () => {
+    cy.visit('http://localhost:3000');
+    cy.get('input[type="file"]').attachFile('consumptions.csv');
 
-        cy.get('.apexcharts-canvas').contains('02/12/2021');
-    });
+    cy.get('.apexcharts-canvas').contains('02/12/2021');
+    cy.get('#SvgjsRect1128').invoke('attr', 'val').should('eq', '1.692');
+  });
 });
