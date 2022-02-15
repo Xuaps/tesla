755e01eab036c7f3f747afdeeef0a826d3b81cf4
David Vílchez <dvilchez@xuaps.com>
2022-02-15 16:46:13 +0100

    Fix cypress test

--- app/cypress/integration/load-consumption.spec.ts
+++ app/cypress/integration/load-consumption.spec.ts
@@ -1,5 +1,15 @@
 describe('Load CNMC consumptions', () => {
   it('show a consumption graph', () => {
+    cy.intercept('/prices/2.0TD/*', (req) => {
+      const urlParts = req.url.split('/');
+      const date = urlParts[urlParts.length - 1].replace('.json', '');
+      const prices = [...(Array(24) as any).keys()].reduce(
+        (acc, hour) => ({ ...acc, [`${date}T${('0' + hour).slice(-2)}:00:00+00:00`]: 0.123 }),
+        {},
+      );
+
+      req.reply(prices);
+    });
     cy.visit('http://localhost:3000');
     cy.get('input[type="file"]').attachFile('consumptions.csv');
 
