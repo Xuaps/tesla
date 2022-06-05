c7440ba02765def4cbff4286c098e8067059bff6
David Vílchez <dvilchez@xuaps.com>
2022-06-05 19:35:00 +0200

    Replace app insight with sentry

--- app/src/components/App.tsx
+++ app/src/components/App.tsx
@@ -1,8 +1,6 @@
 import React, { ChangeEvent, useState } from 'react';
 import { Container, Row, Spinner } from 'react-bootstrap';
 import { useTranslation } from 'react-i18next';
-import { AppInsightsErrorBoundary } from '@microsoft/applicationinsights-react-js';
-import { reactPlugin } from '../AppInsights';
 import us from '../user-stories';
 import { EMPTY_CONSUMPTION, EMPTY_STORE, Store } from '../store';
 import TotalConsumption from './TotalConsumption';
@@ -36,37 +34,32 @@ const App = (): JSX.Element => {
 
   return (
     <Container className="App">
-      <AppInsightsErrorBoundary
-        onError={() => <ErrorPage />}
-        appInsights={reactPlugin}
-      >
-        <Header />
-        <Configuration fileLoaded={fileLoaded} />
-        {isLoading && (
-          <>
-            <Spinner animation="border" variant="primary">
-              <span className="visually-hidden">{t('loading')}</span>
-            </Spinner>
-            &nbsp;{t('loading')}
-          </>
-        )}
-        {store.consumptions !== EMPTY_CONSUMPTION ? (
-          <>
-            <Row className="sparkboxes mt-4 mb-4">
-              <TotalConsumption consumptions={store.consumptions} />
-            </Row>
-            <Row className="sparkboxes mt-4 mb-4">
-              <PuntaSummary consumptions={store.consumptions} />
-              <LlanoSummary consumptions={store.consumptions} />
-              <ValleSummary consumptions={store.consumptions} />
-            </Row>
-            <Heatmap consumptions={store.consumptions} />
-            <Segments consumptions={store.consumptions} />
-          </>
-        ) : (
-          <StartPage />
-        )}
-      </AppInsightsErrorBoundary>
+      <Header />
+      <Configuration fileLoaded={fileLoaded} />
+      {isLoading && (
+        <>
+          <Spinner animation="border" variant="primary">
+            <span className="visually-hidden">{t('loading')}</span>
+          </Spinner>
+          &nbsp;{t('loading')}
+        </>
+      )}
+      {store.consumptions !== EMPTY_CONSUMPTION ? (
+        <>
+          <Row className="sparkboxes mt-4 mb-4">
+            <TotalConsumption consumptions={store.consumptions} />
+          </Row>
+          <Row className="sparkboxes mt-4 mb-4">
+            <PuntaSummary consumptions={store.consumptions} />
+            <LlanoSummary consumptions={store.consumptions} />
+            <ValleSummary consumptions={store.consumptions} />
+          </Row>
+          <Heatmap consumptions={store.consumptions} />
+          <Segments consumptions={store.consumptions} />
+        </>
+      ) : (
+        <StartPage />
+      )}
     </Container>
   );
 };
--- app/src/index.tsx
+++ app/src/index.tsx
@@ -1,16 +1,23 @@
 import React, { Suspense } from 'react';
 import ReactDOM from 'react-dom';
+import * as Sentry from '@sentry/react';
+import { BrowserTracing } from '@sentry/tracing';
 import App from './components/App';
-import { withAITracking } from '@microsoft/applicationinsights-react-js';
-import { reactPlugin } from './AppInsights';
 
 import './i18n';
-
 import 'bootstrap/dist/css/bootstrap.min.css';
 
+Sentry.init({
+  dsn: 'https://f2107223361646888197f11c1e47c63a@o100511.ingest.sentry.io/6471716',
+  integrations: [new BrowserTracing()],
+
+  // Set tracesSampleRate to 1.0 to capture 100%
+  // of transactions for performance monitoring.
+  // We recommend adjusting this value in production
+  tracesSampleRate: 1.0,
+});
+
 ReactDOM.render(
-  <Suspense fallback="...">
-    {React.createElement(withAITracking(reactPlugin, App))}
-  </Suspense>,
+  <Suspense fallback="...">{React.createElement(App)}</Suspense>,
   document.getElementById('root'),
 );
